84ee49fcaf7e341f6af830b79d3cabc8894622ae
Austin Sears <snowyparadox91@gmail.com>
2023-11-13 16:44:55 -0500

    11/13 Revision

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('login.html')
 
 @app.route('/login')
 def login():
@@ -13,6 +13,18 @@ def login():
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
+
+@app.route('/create')
+def create():
+    return render_template('Profile_Create.html')
+
+@app.route('/post')
+def post():
+    return render_template('Post_Page.html')
+
+@app.route('/feed')
+def feed():
+    return render_template('Feed_page.html')
 
 if __name__ == "__main__":
     app.run()
