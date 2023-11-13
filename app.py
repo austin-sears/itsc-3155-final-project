5742ec0c0de99a283aa029e1e3fe9cb9cf38dce5
austin-sears <snowyparadox91@gmail.com>
2023-11-12 23:04:21 -0500

    11/12 Designs

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+if __name__ == "__main__":
+    app.run()
