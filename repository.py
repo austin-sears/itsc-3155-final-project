53dcc650742cca1b7115bbeec8c44786673f05fe
Austin Sears <snowyparadox91@gmail.com>
2023-12-06 16:37:49 -0500

    Repository Resurrection

--- repository.py
+++ repository.py
@@ -8,11 +8,6 @@ db = firestore.client()
 
 currentUser = {}
 
-#
-#TODO - FRONT END TEAM
-#TRANSFER ALL VARIABLES TO MATCH THE NAMES USED IN THESE FUNCTIONS
-#
-
 #sets all the data in repository, returning the id for the document that contains all of the information
 def create_account(Username, Email, AboutMe, Password):
     user = auth.create_user(email = Email, password = Password)
@@ -40,8 +35,7 @@ def get_one_post(post_id):
     else:
         return None
 
-#TODO
-#Create a function that gets all comments from database
+#Matts comment functions
 def get_comments(post_id):
     comments = []
     comment_ref = db.collection('Comments').where('post_id', '==', post_id)
@@ -51,9 +45,6 @@ def get_comments(post_id):
         comments.append(comment_data)
     return comments
 
-
-#TODO
-#Create a function that adds a comment to a given post
 def add_comment(post_id, commenter_username, comment_text):
     new_comment_ref = db.collection('Comments').document()
     new_comment_data = {'post_id': post_id,'commenter_username': commenter_username, 'comment_text': comment_text}
@@ -63,17 +54,10 @@ def add_comment(post_id, commenter_username, comment_text):
 
 def create_post(Name, Link, Description, CreatedBy, Code):
     new_post_ref = db.collection('Post').document()
-    new_post_data = {'Name': Name, 'Link': Link, 'Description': Description, 'CreatedBy': CreatedBy, 'Code': Code}
+    new_post_data = {'Name': Name, 'Link': Link, 'Description': Description, 'CreatedBy': CreatedBy, 'Code': Code, 'post_id' : new_post_ref.id}
     new_post_ref.set(new_post_data)
     new_post_id = new_post_ref.id
     return new_post_id
-
-
-
-def delete_account(acct_id):
-    acct_ref = db.collection('Users').document(acct_id)
-    acct_ref.delete()
-    return None
 
 
 
@@ -94,17 +78,39 @@ def verify(email, password):
         print (user.uid)
         ref = db.collection('Users').document(user.uid).get().to_dict()
         print (ref)
-        if ref['Email'] == email and ref['Password'] == password:
+        if user and ref['Password'] == password:
             print("Login Successful")
-            currentUser['uid'] = user.uid
-            currentUser['Username'] = ref['Username']
-            currentUser['Email'] = ref['Email']
-            currentUser['Password'] = ref['Password']
-            currentUser['AboutMe'] = ref['AboutMe']
-            return currentUser
+            sessionToken = auth.create_custom_token(user.uid)
+            print(sessionToken)
+            return user.uid
         else: 
             print("Invalid Email or Password")
     except Exception as e:
         print("Authentication Failed: ", str(e))
         return None
 
+def get_user_posts(username):
+    users_posts = []
+    posts_docs = db.collection('Post').where('CreatedBy', '==', username).get()
+    for doc in posts_docs:
+        post_data = doc.to_dict()
+        users_posts.append(post_data)
+    return users_posts
+
+
+
+def logout_user(uid):
+    auth.revoke_refresh_tokens(uid)
+    return None
+
+def delete_user(uid):
+    auth.revoke_refresh_tokens(uid)
+    db.collection('Users').document(uid).delete(uid).delete()
+    auth.delete_user(uid)
+    return None
+
+def delete_post(post_id):
+    post_doc = db.collection('Post').document(post_id)
+    print(post_doc)
+    post_doc.delete()
+    return None
